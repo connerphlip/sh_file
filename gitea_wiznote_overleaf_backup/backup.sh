afdf71e6bab3cafbdd152a7a039a2e3b5270121d
connerphlip <connerphlip@gmail.com>
2022-03-14 22:37:02 +0800

    resolve some cp bug

--- gitea_wiznote_overleaf_backup/backup.sh
+++ gitea_wiznote_overleaf_backup/backup.sh
@@ -1,29 +1,29 @@
 cur_date=`date +%Y%m%d`
 cd /home/ganghuic/gitea/gitea_backup
 mkdir $cur_date
-cp ../custom ./$cur_date
-cp ../data ./$cur_date
-cp ../gitea ./$cur_date
-cp ../log ./$cur_date
+cp -afdpr ../custom ./$cur_date
+cp -afdpr ../data ./$cur_date
+cp -afdpr ../gitea ./$cur_date
+cp -afdpr ../log ./$cur_date
 
 
 cd /home/ganghuic/wizdata/wizdata_backup
 mkdir $cur_date
-cp ../data_root ./$cur_date
-cp ../db ./$cur_date
-cp ../index ./$cur_date
-cp ../log ./$cur_date
-cp ../override_files ./$cur_date
-cp ../store ./$cur_date
-cp ../wiz-abstract ./$cur_date
-cp ../wiz-gather-dev ./$cur_date
-cp ../wizserver-temp ./$cur_date
+cp -afdpr ../data_root ./$cur_date
+cp -afdpr ../db ./$cur_date
+cp -afdpr ../index ./$cur_date
+cp -afdpr ../log ./$cur_date
+cp -afdpr ../override_files ./$cur_date
+cp -afdpr ../store ./$cur_date
+cp -afdpr ../wiz-abstract ./$cur_date
+cp -afdpr ../wiz-gather-dev ./$cur_date
+cp -afdpr ../wizserver-temp ./$cur_date
 
 cd /home/ganghuic/overleaf/overleaf_backup
 mkdir $cur_date
-cp ../docker-compose.yml ./$cur_date
-cp ../mongo_data ./$cur_date
-cp ../redis_data ./$cur_date
-cp ../sharelatex_data ./$cur_date
+cp -afdpr ../docker-compose.yml ./$cur_date
+cp -afdpr ../mongo_data ./$cur_date
+cp -afdpr ../redis_data ./$cur_date
+cp -afdpr ../sharelatex_data ./$cur_date
 
 
